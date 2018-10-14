92eb9d69bd93e96594717a9df9f68b5e270001dd
kamil <kammmilkos@gmail.com>
2018-10-14 11:01:28 +0200

    Complete Graham's algorithm

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # AADS
+
+## Advanced algorithms and data structures.
+Implemented algorithms:  
+ - lab1 - Graham's algorithm
+ - lab2 - X
+ - lab3 - X
+ - lab4 - X
+ - lab5 - X
+ - lab6 - X
+ - lab7 - X
--- lab1/main.py
+++ lab1/main.py
@@ -2,13 +2,15 @@ import matplotlib.pyplot as plt
 from point import Point
 from operator import attrgetter
 from stack import Stack
+from copy import deepcopy
+import pandas as pd
 
 
 def draw_points(points_all, points_hull):
     pax, pay = [p.x for p in points_all], [p.y for p in points_all]
     phx, phy = [p.x for p in points_hull], [p.y for p in points_hull]
     plt.plot(pax, pay, 'b', linewidth=0, marker='o')
-    plt.plot(phx, phy, 'r', linewidth=0, marker='o')
+    plt.plot(phx, phy, 'r', linewidth=1, marker='o')
     plt.show()
 
 
@@ -17,6 +19,7 @@ def ccw(p1, p2, p3):
 
 
 def transform_points(points):
+    points = deepcopy(points)
     min_y = min(points, key=lambda x: x.y).y
     ref_point = min(filter(lambda p: p.y == min_y, points), key=attrgetter('x'))
     vector = Point(-ref_point.x, -ref_point.y)
@@ -29,22 +32,17 @@ def transform_points(points):
             break
 
     point_tmp = Point(points_ref[0].x, points_ref[0].y)
-    points_ref[0].x = ref_point.x
-    points_ref[0].y = ref_point.y
+    points_ref[0].set_coordinates(ref_point.x, ref_point.y)
 
-    ref_point.x = point_tmp.x
-    ref_point.y = point_tmp.y
+    ref_point.set_coordinates(point_tmp.x, point_tmp.y)
 
-    points_polar = [p.convert_polar() for p in points_ref]
-    points_polar_sorted = sorted(points_polar, key=lambda p: (p.phi, p.r))
-    points_cartesian_sorted = [p.convert_cartesian() for p in points_polar_sorted]
-    return [Point(p.x - vector.x, p.y - vector.y) for p in points_cartesian_sorted]
+    points_polar_sorted = sorted(points_ref, key=lambda p: (p.phi, p.r))
+    points_polar_sorted.append(points_polar_sorted[0])
+    return [Point(p.x - vector.x, p.y - vector.y) for p in points_polar_sorted]
 
 
 def graham_hull(points):
     points = transform_points(points)
-
-    print(points)
 
     stack = Stack()
     stack.append(points[0])
@@ -60,25 +58,11 @@ def graham_hull(points):
 
 
 def main():
-    points = [
-        Point(0, -1),
-        Point(0, 0),
-        Point(0, 1),
+    points = []
+    points_df = pd.read_csv('punktyPrzykladowe.csv', sep=',')
+    for i in range(len(points_df)):
+        points.append(Point(points_df.iloc[i, 0], points_df.iloc[i, 1]))
 
-        Point(1, -1),
-        Point(1, 0),
-        Point(1, 1),
-
-        Point(2, -1),
-        Point(2, 0),
-        Point(2, 1),
-
-        Point(3, -1),
-        Point(3, 0),
-        Point(3, 1)
-    ]
-
-    points.reverse()
     points_hull = graham_hull(points)
     draw_points(points, points_hull)
 
